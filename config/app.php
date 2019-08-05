da46ff5ef72fa5c68fedb60e46b5485d5460bc76
Akshay DNB <akshay.makasana@dnb-ma.com>
2019-08-05 17:03:57 +0530

    2019-08-05
    set live config

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://php72dnb.local'),
+    'url' => env('APP_URL', 'https://opticalerp.com'),
 
     /*
     |--------------------------------------------------------------------------
--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'loaclhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dbopw'),
+            'username' => env('DB_USERNAME', 'dbopw'),
+            'password' => env('DB_PASSWORD', '(YKQsJGae7vR'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
